eb80611d4b446ff77606d836bfd159bb2d5a2e95
apbatth062 <batthamrit04@gmail.com>
2019-11-20 22:13:27 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var query;
+window.onload = function () {
+    $.get(`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=40f75048a7984d86bcbf8feadfadbcb5`,
+        function (data, status) {
+
+
+            for (let i = 0; i < 8; i++) {
+                $('ul').append('<li class="title">' + '<h3>' + data.articles[i].title + '</h3>' + '</li>')
+                $('ul').append('<li class="list-group-item">' + data.articles[i].description + '</li>')
+                $('ul').append('<hr>')
+                
+            }
+        });
+
+}
+
+//for searching country news
+
+$(document).ready(function () {
+    $("#searchC").click(function () {
+    
+        query = $("#query").val();
+
+
+        $.get(`https://newsapi.org/v2/top-headlines?country=${query}&category=business&apiKey=40f75048a7984d86bcbf8feadfadbcb5`,
+            function (data, status) {
+               $('ul').remove();
+                $('.ResultNews').append('<ul> </ul>')
+                for (let i = 0; i < 5; i++) {
+                    $('ul').append('<li class="title">' + '<h3>' + data.articles[i].title +
+                        '</h3>' + '</li>')
+                    $('ul').append('<li class="list-group-item">' + data.articles[i].description +
+                        '</li>')
+                    $('ul').append('<hr>')
+                    
+                }
+            });
+    });
+});
+
+
